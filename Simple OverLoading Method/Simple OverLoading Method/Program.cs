6387353e7b3fb28133b08d81a1bf4ceee1ccf96a
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2022-09-01 00:53:37 +0430

    Add files via upload

---
+++ Simple OverLoading Method/Simple OverLoading Method/Program.cs
@@ -0,0 +1,45 @@
+﻿//Navid-Derakhshandeh
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+
+    class Rectangle
+    {
+        private double n;
+
+        public Rectangle(double x)
+        {
+            this.n = x;
+
+        }
+        public static double operator *(Rectangle s1, Rectangle s2)
+        {
+            return s1.n * s2.n;
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo conkey;
+            double x;
+            double xn;
+            do
+            {
+                Console.WriteLine("Enter the number of length");
+                x = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the number of width ");
+                xn = Double.Parse(Console.ReadLine());
+
+                Rectangle newrect1 = new Rectangle(x);
+
+                Console.WriteLine("Space equal by {0}.", x * xn);
+
+                conkey = Console.ReadKey();
+            } while (conkey.Key != ConsoleKey.Escape);
+        }
+    }
+}
